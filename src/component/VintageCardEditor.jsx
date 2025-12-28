78ace3f4db7509c80f56e2f57a2b7535543f24e7
shashwa7-dev <shashwa7.dev@gmail.com>
2025-12-28 17:33:25 +0530

    product hunt embed added

--- src/component/VintageCardEditor.jsx
+++ src/component/VintageCardEditor.jsx
@@ -117,7 +117,6 @@ export default function VintageCardEditor() {
           {"]"}
         </a>
       </div>
-
       <div className="editor-layout">
         {/* CARD PREVIEW */}
         <div className="preview-section">
@@ -322,6 +321,19 @@ export default function VintageCardEditor() {
             )}
           </div>
         </div>
+      </div>
+      <div className="prod_hunt">
+        <a
+          href="https://www.producthunt.com/products/papernoise?embed=true&amp;utm_source=badge-featured&amp;utm_medium=badge&amp;utm_campaign=badge-papernoise"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="PaperNoise - Design cards that feel discovered, not designed. | Product Hunt"
+            width="150"
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1055230&amp;theme=neutral&amp;t=1766895689630"
+          />
+        </a>
       </div>
     </div>
   );
